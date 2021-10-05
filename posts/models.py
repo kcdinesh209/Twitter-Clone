4caf222ad5f2a53d2117d28121030e821de8aa58
kcdinesh209 <kcdinesh209@gmail.com>
2021-10-05 03:46:06 -0400

    First Commit

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+import cloudinary
+from django.db import models
+from cloudinary.models import CloudinaryField
+# Create your models here.
+class Post (models.Model):
+    class Meta(object):
+        db_table='post'
+    name=models.CharField('Name',blank=False,null=False,max_length=14,db_index=True,default='Anonymous')
+    body=models.CharField('Body',blank=True,null=True,max_length=140,db_index=True) 
+    created_at=models.DateTimeField('Created Date Time',blank=True,auto_now_add=True)  
+    image = CloudinaryField('image', blank=True, null=True)
+    likes = models.PositiveIntegerField(
+        'like',default=0,blank=True,db_index= True
+    )
