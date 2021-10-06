4d0f5beccb57e63e799c0de35f74dff627bc4bed
kcdinesh209 <kcdinesh209@gmail.com>
2021-10-06 00:53:02 -0400

    comments removed from posts.js

--- static/JS/posts.js
+++ static/JS/posts.js
@@ -1,11 +1,9 @@
-//// Java Script for Post page
+
 $(function(){
 
-    //Execute when js-menu-icon is clicked
+    
     $('.js-menu_icon').click(function(){
-        // $(this) : self element, namely div.js-menu-icon
-        //next():Next to div.js-menu-icon namely div.menu
-        // toggle(): switch show and hide
+       
         $(this).next().toggle();
     })
 })
